ffb304349d73159f6a858471e1bf8376d5326488
IMH51 <ianmartinharrison@outlook.com>
2018-11-28 14:48:20 +0000

    created custom triangleerror class to be called in the event of an error in the kind method

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if ((self.a || self.b || self.c) <= 0) | (self.a + self.b <= self.c || self.b + self.c <= self.a || self.a + self.c <= self.b)
+        raise TriangleError
+    elsif self.a == self.b && self.b == self.c && self.c == self.a
+      return :equilateral
+    elsif
+      self.a == self.b || self.b == self.c || self.c == self.a
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
